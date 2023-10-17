248dedd0dc799507eba69a1d6c0ed02c303f8ff5
pengsuming <w2212@e-mirim.hs.kr>
2023-10-17 14:22:05 +0900

    [add] : 회원가입 받는거 만드는 중

--- src/main/java/org/example/login.java
+++ src/main/java/org/example/login.java
@@ -1,10 +1,54 @@
 package org.example;
 
+import java.util.Scanner;
+
+class User {
+    private String id;
+    private String pw;
+
+    public User(String id, String pw) {
+        setId(id);
+        setPw(pw);
+    }
+    public User(String id) {
+        setId(id);
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPw() {
+        return pw;
+    }
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+    @Override
+    pubilc boolean equals(Object o) {
+        if(o == null || !(o instanceof User)) {
+            return false;
+        }
+        User temp = (User)o;
+
+        return id.equals(temp.getId());
+    }
+    @Override
+    public String toString() {
+        String info = "Id: " + id + "\n";
+        info += "Pw: " + pw + "\n";
+        return info;
+    }
+
+}
+
 public class login extends frame{
     public login(){
 
     }
     public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
         login frame = new login();
     }
 }

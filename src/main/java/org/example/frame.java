b17f81af05bc04ae3a3081bba0a0ec8b5df6e1b7
pengsuming <88700168+leeda06@users.noreply.github.com>
2023-10-17 16:30:01 +0900

    [add] : gameStart파일 추가

--- src/main/java/org/example/frame.java
+++ src/main/java/org/example/frame.java
@@ -3,8 +3,11 @@ import javax.swing.JFrame;
 
 public class frame extends JFrame{
     public frame(){
+        int frame_width = 1080;
+        int frame_height = 720;
+
         setTitle("원시 초밥");
-        setSize(1080, 720);
+        setSize(frame_width, frame_height);
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/main/java/org/example/gameStart.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class gameStart extends frame{
+    public gameStart(){
+
+    }
+    public static void main(String[] args) {
+        gameStart frame = new gameStart();
+    }
+}

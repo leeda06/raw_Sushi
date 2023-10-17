7a3a4507397453fa149a6a303a4ff6f78f471396
pengsuming <w2212@e-mirim.hs.kr>
2023-10-17 14:02:15 +0900

    [add] : 로그인 프레임 추가

--- src/main/java/org/example/login.java
+++ src/main/java/org/example/login.java
@@ -1,5 +1,10 @@
 package org.example;
 
-public class login {
-    
+public class login extends frame{
+    public login(){
+
+    }
+    public static void main(String []args){
+        login frame = new login();
+    }
 }

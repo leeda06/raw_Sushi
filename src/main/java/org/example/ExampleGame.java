41962ff0153c761d66ba6fac2dde39cbc758fe40
leeda06 <88700168+leeda06@users.noreply.github.com>
2023-10-26 09:17:13 +0900

    [Repacktory] : 파일 내 이미지 경로 간편화

--- src/main/java/org/example/ExampleGame.java
+++ src/main/java/org/example/ExampleGame.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class ExampleGame extends Frame{
     public ExampleGame(){
-        background = new JLabel(new ImageIcon(Main.class.getResource(Frame.LINK+"Main_/back_main.png")));
-        background.setBounds(0 , 0 , Frame.WIDTH, Frame.HEIGHT);
+        background = new JLabel(new ImageIcon(Main.class.getResource(LINK+"Main_/back_main.png")));
+        background.setBounds(0 , 0 , WIDTH, HEIGHT);
         add(background);
     }
 

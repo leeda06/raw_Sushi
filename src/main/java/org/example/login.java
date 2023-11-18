c9a48dd17615aaeed8f63991a9865f146f74cc9f
leeda06 <88700168+leeda06@users.noreply.github.com>
2023-11-18 17:20:15 +0900

    [add] : login페이지
    - 기본 db에 있던 아이디도 중복 불가

--- src/main/java/org/example/login.java
+++ src/main/java/org/example/login.java
@@ -1,28 +1,27 @@
 package org.example;
-
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class login extends Frame {  // JFrame을 상속받도록 수정
+public class login extends Frame {
     private JTextField iDTextInput = new JTextField(20);
     private JTextField pWTextInput = new JTextField(20);
 
     private ArrayList<String> idList = new ArrayList<>();
+    private ArrayList<String> pwList = new ArrayList<>();
     private String DATABASE_FILE = "user_database.txt";
 
     public login(){
 
         getContentPane().setBackground(Color.ORANGE);
 
-        iDTextInput.setBounds((getWidth() - 450) / 2 , getHeight() - 139 - (70 + (93*3)), 450, 70);  // getWidth()와 getHeight()로 수정
-        pWTextInput.setBounds((getWidth() - 450) / 2 , getHeight() - 139 - (93*2)- 20, 450, 70);  // getWidth()와 getHeight()로 수정
+        iDTextInput.setBounds((Frame.WIDTH - 450) / 2 , Frame.HEIGHT - 139 - (70 + (93*3)), 450, 70);
+        pWTextInput.setBounds((Frame.WIDTH - 450) / 2 , Frame.HEIGHT - 139 - (93*2)- 20, 450, 70);
 
-        Font newFont = new Font("나눔체", Font.PLAIN, 30);
+        Font newFont = new Font("나눔체", Font.PLAIN, 30); // 여기에서 "Arial"은 원하는 폰트 이름이며, 20은 원하는 크기입니다.
         iDTextInput.setFont(newFont);
         pWTextInput.setFont(newFont);
 
@@ -32,8 +31,8 @@ public class login extends Frame {  // JFrame을 상속받도록 수정
         JLabel iDText = new JLabel("아이디");
         JLabel pWText = new JLabel("비밀번호");
 
-        iDText.setBounds(((getWidth() - 450) / 2 ),(getHeight() - 139 - (70 + (93*4))-140) , 500, 400);
-        pWText.setBounds(((getWidth() - 450) / 2 ),(getHeight() - 139 - (70 + (93*3))-90) , 500, 400);
+        iDText.setBounds(((Frame.WIDTH - 450) / 2 ),(Frame.HEIGHT - 139 - (70 + (93*4))-140) , 500, 400);
+        pWText.setBounds(((Frame.WIDTH - 450) / 2 ),(Frame.HEIGHT - 139 - (70 + (93*3))-90) , 500, 400);
 
         iDText.setFont(newFont);
         pWText.setFont(newFont);
@@ -42,18 +41,20 @@ public class login extends Frame {  // JFrame을 상속받도록 수정
         add(pWText);
 
         JButton iDButton = new JButton("로그인");
-        iDButton.setBounds((getWidth() / 2) - 225, getHeight() - 230, 220, 70);
+        iDButton.setBounds((Frame.WIDTH/2) - 225, Frame.HEIGHT - 230, 220, 70);
         iDButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 String idText = iDTextInput.getText();
                 String pwText = pWTextInput.getText();
                 if (authenticate(idText, pwText)) {
+                    // 로그인 성공 처리
                     JOptionPane.showMessageDialog(null, "로그인 성공");
                     Frame.ID = idText;
                     new Main();
                     setVisible(false);
                 } else {
+                    // 로그인 실패 처리
                     JOptionPane.showMessageDialog(null, "로그인 실패");
                 }
             }
@@ -61,7 +62,7 @@ public class login extends Frame {  // JFrame을 상속받도록 수정
         add(iDButton);
 
         JButton pWButton = new JButton("회원가입");
-        pWButton.setBounds((getWidth() / 2) + 5, getHeight() - 230, 220, 70);
+        pWButton.setBounds((Frame.WIDTH/2) + 5, Frame.HEIGHT - 230, 220, 70);
         pWButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -82,11 +83,7 @@ public class login extends Frame {  // JFrame을 상속받도록 수정
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts[0].equals(username) && parts[1].equals(password)) {
-                    Frame.TEMA = Integer.parseInt(parts[4]);
-                    Frame.ITEM = Integer.parseInt(parts[3]);
-                    Frame.SCORE = Integer.parseInt(parts[2]);
-                    updateLastAttendanceDate(username); // 로그인 시 출석일 업데이트
+                if (parts.length == 2 && parts[0].equals(username) && parts[1].equals(password)) {
                     return true;
                 }
             }
@@ -96,23 +93,27 @@ public class login extends Frame {  // JFrame을 상속받도록 수정
         return false;
     }
 
-    private void updateLastAttendanceDate(String username) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATABASE_FILE, true))) {
-            String currentDate = LocalDate.now().minusDays(1).toString(); // 오늘보다 하루 전의 날짜를 마지막 출석일로 설정
-            writer.write(username + "," + currentDate);
-            writer.newLine();
+    private boolean addUser(String username, String password) {
+        try (BufferedReader br = new BufferedReader(new FileReader(DATABASE_FILE))) {
+            String line;
+
+            // 파일의 각 줄에 대해 반복
+            while ((line = br.readLine()) != null) {
+                // 각 줄을 쉼표로 분리하여 첫 번째 아이디를 추출
+                String[] tokens = line.split(",");
+                String firstId = tokens[0];
+
+                // idList에 추가
+                idList.add(firstId);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private boolean addUser(String username, String password) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATABASE_FILE, true))) {
             if (idList.contains(username)) {
                 return false; // 이미 존재하는 아이디
             } else {
-                String currentDate = LocalDate.now().minusDays(1).toString(); // 오늘보다 하루 전의 날짜를 마지막 출석일로 설정
-                writer.write(username + "," + password + "," + 0 + "," + 0 + "," + -1 + "," + currentDate);
+                writer.write(username + "," + password);
                 writer.newLine();
                 idList.add(username);
                 return true;
